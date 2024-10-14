e7f1e08f301ae25043e52f5be5d74f3083f2a17f
algoscienceacademy <sshahrearhossain@gmail.com>
2024-10-14 13:19:41 +0600

    update v01

---
+++ C++_solution/solution_8.cpp
@@ -0,0 +1,34 @@
+/*
+* Problem 8: Check Whether a given year is a leap year
+* Author: Shahrear Hossain
+* github: algoscienceacademy
+* Cradit: Manish Bhojasia
+* Date: 14 October 2024
+* GPL-Lassar Licence
+
+*/
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(int args, const char* argv[])
+//{
+//	int year;
+//	cout << "Enter a year";
+//	cin >> year;
+//
+//	if ((year % 400 == 0 || year % 4 == 0) && year % 100 != 0)
+//	{
+//		cout << year << "is a leap year" << endl;
+//	}
+//	else 
+//	{
+//		cout << year << " is not a leap year" << endl;
+//
+//	}
+//
+//	return 0;
+//}
+//
+
---
+++ C++_solution/solution_9.cpp
@@ -0,0 +1,10 @@
+/*
+* Problem 9: Check Wheither a Chracter is a Vowel, Consonant or Digit
+* Author: Shahrear Hossain
+* github: algoscienceacademy
+* Cradit: Manish Bhojasia
+* Date: 10 October 2024
+* GPL-Lassar Licence
+
+*/
+
